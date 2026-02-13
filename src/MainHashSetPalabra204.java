8e2a6f8911f73e78c6bdf3d25b32d305046b4bba
pablo <pablo.hergue@educa.jcyl.es>
2026-02-13 08:51:29 +0100

    commit extra

--- src/MainHashSetPalabra204.java
+++ src/MainHashSetPalabra204.java
@@ -1,13 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Arrays;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class MainHashSetPalabra204 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce una lista de palabras separadas por espacios:");
+        String entrada = scanner.nextLine();
+
+        String[] todasLasPalabras = entrada.split(" ");
+
+        HashSet<String> palabrasUnicas = new HashSet<>(Arrays.asList(todasLasPalabras));
+
+        System.out.println("\nPalabras únicas encontradas:");
+        for (String palabra : palabrasUnicas) {
+            System.out.println("- " + palabra);
+        }
+
+        scanner.close();
+    }
 }
